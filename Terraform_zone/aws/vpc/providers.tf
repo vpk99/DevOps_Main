1a2e2578c8e09f968865ce70498a5c19dcb1af2f
khotvp <devopsandcloud24@gmail.com>
2024-05-22 17:13:27 +0530

    added backend

--- Terraform_zone/aws/vpc/providers.tf
+++ Terraform_zone/aws/vpc/providers.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+# Storing state files in s3 bucket 
+terraform {
+  backend "s3" {
+    bucket = "tterraformstatebackend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    # Enable state locking (recommended)
+    dynamodb_table = "terraformbackend"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
   default_tags {
