9f81a2e3084b9c9a543c88684c68aaeac3b99604
khotvp <devopsandcloud24@gmail.com>
2024-05-23 17:47:29 +0530

    added first version of module in azure

--- Terraform_zone/azure/modules/vnet/inputs.tf
+++ Terraform_zone/azure/modules/vnet/inputs.tf
@@ -1,10 +1,30 @@
 variable "network_info" {
-  type = list(object({
-    name = string
-    
-  }))
+type = object
+description = "virtual network information"
+default = {
+  name = "ntier"
+  location = "eastus"
+  address_space = "10.0.0.0/16"
+
+}
 }
 
+
 variable "subnet_info" {
-  
+  type = object({
+    name = list(string)
+    cidr = list(string)
+    nsg_rules = list(object({
+      name = string
+      description = string
+      source_port_range = string
+      destination_port_range = string
+      source_address_prefix = string
+      destination_address_prefix = string
+      access = string
+      priority = string
+      direction = string
+    }))
+
+  })
 }
--- Terraform_zone/azure/modules/vnet/main.tf
+++ Terraform_zone/azure/modules/vnet/main.tf
@@ -1,19 +1,35 @@
 resource "azurerm_virtual_network" "ntier-primary" {
-  name                = var.network_name
+  name                = var.network_info.name
   resource_group_name = azurerm_resource_group.ntier.name
-  location            = var.location
-  address_space       = var.network_cidr
+  location            = var.network_info.location
+  address_space       = var.network_info.address_space
 
-  depends_on = [azurerm_resource_group.ntier]
+  
 }
 
 resource "azurerm_subnet" "subnets" {
-  count                = length(var.subnet_names)
-  name                 = var.subnet_names[count.index]
+  count                = length(var.subnet_info)
+  name                 = var.subnet_info[count.index].name
   resource_group_name  = azurerm_resource_group.ntier.name
-  virtual_network_name = azurerm_virtual_network.ntier-primary.name
-  address_prefixes     = [var.subnet_cidrs[count.index]]
+  virtual_network_name = var.network_info.name
+  address_prefixes     = var.subnet_info[count.index].address_prefixes
 
-  depends_on = [azurerm_virtual_network.ntier-primary]
+
 }
+
+# Creating network security group rules
+resource "azurerm_network_security_rule" "web" {
+  count                       = length(var.subnet_info)
+  name                        = var.subnet_info[count.index].name
+  resource_group_name         = azurerm_resource_group.ntier.name
+  network_security_group_name = azurerm_network_security_group.web.name
+  description                 = var.subnet_info[count.index].description
+  protocol                    = var.subnet_info[count.index].protocol
+  source_port_range           = var.subnet_info[count.index].source_port_range
+  destination_port_range      = var.subnet_info[count.index].destination_port_range
+  source_address_prefix       = var.subnet_info[count.index].source_address_prefix
+  destination_address_prefix  = var.subnet_info[count.index].destination_address_prefix
+  access                      = var.subnet_info[count.index].access
+  priority                    = var.subnet_info[count.index].priority
+  direction                   = "Inbound"
 
