2856a6647fb5dc1d69401fd7c9201274dd073c8d
khotvp <devopsandcloud24@gmail.com>
2024-05-24 12:07:14 +0530

    discard all changes

--- Terraform_zone/azure/vnet/network.tf
+++ Terraform_zone/azure/vnet/network.tf
@@ -26,3 +26,21 @@ resource "azurerm_subnet" "subnets" {
   depends_on = [azurerm_virtual_network.ntier-primary]
 }
 
+# Creating network security group rules
+resource "azurerm_network_security_rule" "web" {
+  count                       = length(var.web_nsg_rules)
+  name                        = var.web_nsg_rules[count.index].name
+  resource_group_name         = azurerm_resource_group.ntier.name
+  network_security_group_name = azurerm_network_security_group.web.name
+  description                 = var.web_nsg_rules[count.index].description
+  protocol                    = var.web_nsg_rules[count.index].protocol
+  source_port_range           = var.web_nsg_rules[count.index].source_port_range
+  destination_port_range      = var.web_nsg_rules[count.index].destination_port_range
+  source_address_prefix       = var.web_nsg_rules[count.index].source_address_prefix
+  destination_address_prefix  = var.web_nsg_rules[count.index].destination_address_prefix
+  access                      = var.web_nsg_rules[count.index].access
+  priority                    = var.web_nsg_rules[count.index].priority
+  direction                   = "Inbound"
+
+  depends_on = [azurerm_network_security_group.web]
+}
--- Terraform_zone/azure/vnet/webserver.tf
+++ Terraform_zone/azure/vnet/webserver.tf
@@ -27,24 +27,7 @@ resource "azurerm_network_security_group" "web" {
   depends_on = [azurerm_resource_group.ntier]
 }
 
-# Creating network security group rules
-resource "azurerm_network_security_rule" "web" {
-  count                       = length(var.web_nsg_rules)
-  name                        = var.web_nsg_rules[count.index].name
-  resource_group_name         = azurerm_resource_group.ntier.name
-  network_security_group_name = azurerm_network_security_group.web.name
-  description                 = var.web_nsg_rules[count.index].description
-  protocol                    = var.web_nsg_rules[count.index].protocol
-  source_port_range           = var.web_nsg_rules[count.index].source_port_range
-  destination_port_range      = var.web_nsg_rules[count.index].destination_port_range
-  source_address_prefix       = var.web_nsg_rules[count.index].source_address_prefix
-  destination_address_prefix  = var.web_nsg_rules[count.index].destination_address_prefix
-  access                      = var.web_nsg_rules[count.index].access
-  priority                    = var.web_nsg_rules[count.index].priority
-  direction                   = "Inbound"
 
-  depends_on = [azurerm_network_security_group.web]
-}
 
 
 # creating network interface 
