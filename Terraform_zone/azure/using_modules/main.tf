70ef55b6d9f7c3bc39784746cfba383174ee15c2
khotvp <devopsandcloud24@gmail.com>
2024-05-24 17:48:43 +0530

    cahanges done

--- Terraform_zone/azure/using_modules/main.tf
+++ Terraform_zone/azure/using_modules/main.tf
@@ -1,17 +1,18 @@
 module "vnet" {
-  source       = "github.com/vpk99/DevOps_Main/Terraform_zone/azure/modules/vnet"
-  network_name = "ntier-primary"
-  network_cidr = ["10.0.0.0/16"]
-  location     = "eastus"
-  subnet_names = ["web1", "web2"]
-  subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  source              = "github.com/vpk99/DevOps_Main/Terraform_zone/azure/modules/vnet"
+  network_name        = "ntier-primary"
+  network_cidr        = ["10.0.0.0/16"]
+  location            = "eastus"
+  subnet_names        = ["web1", "web2"]
+  subnet_cidrs        = ["10.0.1.0/24", "10.0.2.0/24"]
   resource_group_name = "ntier"
 }
 
+
 module "nsg" {
-  source = "../modules/NSG"
+  source                 = "../modules/NSG"
   network_interface_name = "web_nic"
-  Public_ip_name = "web"
+  Public_ip_name         = "web"
   web_nsg_rules = [{
     name                       = "openssh"
     description                = "opens 22 port"
@@ -39,20 +40,24 @@ module "nsg" {
   }]
 }
 
+
+
+
 #Creating a vm using module
 module "vm" {
   source = "../modules/vm"
   web_vm_info = {
-     name           = "vmweb"
-  admin_username = "ubuntu"
-  vm_size        = "Standard_B1s"
-  key_path       = "/home/ubuntu/.ssh/authorized_keys"
-  key_data       = "~/.ssh/id_rsa.pub"
-  disk_type      = "Standard_LRS"
-  publisher      = "canonical"
-  offer          = "0001-com-ubuntu-server-jammy"
-  sku            = "22_04-lts-gen2"
-  version        = "latest"
+    name           = "vmweb"
+    admin_username = "ubuntu"
+    vm_size        = "Standard_B1s"
+    key_path       = "/home/ubuntu/.ssh/authorized_keys"
+    key_data       = "~/.ssh/id_rsa.pub"
+    disk_type      = "Standard_LRS"
+    publisher      = "canonical"
+    offer          = "0001-com-ubuntu-server-jammy"
+    sku            = "22_04-lts-gen2"
+    version        = "latest"
 
   }
 }
+
